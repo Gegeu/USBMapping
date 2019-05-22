10427348756a1c5af049933f9cc86bf75ae04069
Gegeu <48287293+Gegeu@users.noreply.github.com>
2019-05-22 17:33:45 -0300

    Update USBMapping.py

--- USBMapping.py
+++ USBMapping.py
@@ -11,26 +11,25 @@ config = configparser.ConfigParser()
 config.read("\\config.ini.txt")
 
 nome_tecnico = config.get("section1", "nome_tecnico")
-map_local = config.get("section1", "local")
+local_mapeamento = config.get("section1", "local")
 
 def exclui_csv():
     os.remove('\\offline.csv')
     
 def connect():
-    """ Connect to MySQL database """
     try:
-        conn = pymysql.connect(host='127.0.0.1', 
-                                       db='teste-relac',
+        conn = pymysql.connect(host='testebanco.c40hxkoyiz9i.us-east-2.rds.amazonaws.com', 
+                                       db='testebanco',
                                        user='root',
-                                       password='testeteste', autocommit = True)
+                                       password='testeteste')
         
         return conn
     except:
         
-        with open('\\offline.csv', 'w', encoding='utf-8') as f:
+        with open('\\offline.csv', 'a', encoding='utf-8', newline='') as f:
             try:
                 writer = csv.writer(f)
-                writer.writerow((hw_Processador(), hw_Fabricante_PlacaMae(), hw_Modelo_PlacaMae(), hw_NS_PlacaMae(), sis_HostName(), sis_SistemaOperacional(), sis_Data_Instalacao(), rede_IPV4(), rede_MacAddress(), rede_DNS(), hw_Memoria_RAM(), hw_HD(), nome_tecnico))
+                writer.writerow([hw_Processador(), hw_Fabricante_PlacaMae(), hw_Modelo_PlacaMae(), hw_NS_PlacaMae(), sis_HostName(), sis_SistemaOperacional(), sis_Data_Instalacao(), sis_Dominio(), sis_Arquitetura(), sis_Usuario_Atual(), rede_IPV4(), rede_MacAddress(), rede_DNS(), hw_Memoria_RAM(), hw_HD(), nome_tecnico, local_mapeamento])
                 print('SEM CONEXAO! ARQUIVO CSV GERADO.')
             finally:
                 f.close()
@@ -40,7 +39,7 @@ def connect():
 if os.path.isfile('\\offline.csv'):
     
             try:
-                    with open ('\\offline.csv', encoding='utf-8') as off:
+                    with open('\\offline.csv', encoding='utf-8') as off:
                         
                         off_csv = csv.reader(off, delimiter=',')
                         for row in off_csv:
@@ -48,7 +47,7 @@ if os.path.isfile('\\offline.csv'):
                                 continue
                             conn = connect()
                             cursor = conn.cursor()
-                            query = "INSERT INTO maquina(processador, mb_fabricante, mb_modelo, mb_num_serie, sis_hostname, sis_versao_sistema, sis_data_instalacao, rede_ipv4, rede_macaddress, rede_srv_dns, memoria_ram, total_hd, nome_tecnico)VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+                            query = "INSERT INTO maquina(processador, mb_fabricante, mb_modelo, mb_num_serie, sis_hostname, sis_versao_sistema, sis_data_instalacao, sis_Dominio, sis_Arquitetura, sis_Usuario_Atual, rede_ipv4, rede_macaddress, rede_srv_dns, memoria_ram, total_hd, nome_tecnico, local_mapeamento)VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
                             cursor.execute(query, row)                    
                             conn.commit()
                             print('Sucesso ao inserir maquina anterior.')
@@ -56,7 +55,9 @@ if os.path.isfile('\\offline.csv'):
                             conn.close()
             except Exception as erro:
                         print('erro csv: ', erro)
-            exclui_csv()
+            finally:
+                exclui_csv()
+#             exclui_csv()
 
                 
 def hw_Processador():
@@ -145,7 +146,6 @@ def hw_Memoria_RAM():
                         mem = int(interface.TotalPhysicalMemory)
                         mem = (mem  / (1024*1024*1024))
                     return mem	
-                    #print(f'memoria total: {mem:.2f}', 'GB')
             except Exception as erro:
                     print("Não foi possivel identificar o endereco: ", erro)
 
@@ -177,42 +177,30 @@ def sis_Usuario_Atual():
             try:
                     for interface in c.Win32_ComputerSystem():
                         usu_atual = interface.UserName
+                        usu_atual = usu_atual.split('\\')[-1]
                     return usu_atual
             except Exception as erro:
                     print("Não foi possivel identificar o usuario atual: ", erro)
 
-
 connect()
-
     
-def insert_nome(hw_Processador, sis_HostName, hw_Modelo_PlacaMae, hw_Fabricante_PlacaMae, hw_NS_PlacaMae, sis_SistemaOperacional, sis_Data_Instalacao, rede_IPV4, rede_MacAddress, rede_DNS, hw_Memoria_RAM, hw_HD, sis_Dominio, sis_Arquitetura, sis_Usuario_Atual, nome_tecnico, map_local):
-    query_local = "INSERT INTO mapeamento(map_nome_tecnico, map_local)VALUES(%s, %s)"
-    args_local = [nome_tecnico], 4
-
-    
+def insert_nome(hw_Processador, hw_Fabricante_PlacaMae, hw_Modelo_PlacaMae, hw_NS_PlacaMae, sis_HostName, sis_SistemaOperacional, sis_Data_Instalacao, sis_Dominio, sis_Arquitetura, sis_Usuario_Atual, rede_IPV4, rede_MacAddress, rede_DNS, hw_Memoria_RAM, hw_HD, nome_tecnico, local_mapeamento):
+    query = "INSERT INTO maquina(processador, mb_fabricante, mb_modelo, mb_num_serie, sis_hostname, sis_versao_sistema, sis_data_instalacao, sis_Dominio, sis_Arquitetura, sis_Usuario_Atual, rede_ipv4, rede_macaddress, rede_srv_dns, memoria_ram, total_hd, nome_tecnico, local_mapeamento)VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    args = [hw_Processador], [hw_Fabricante_PlacaMae], [hw_Modelo_PlacaMae], [hw_NS_PlacaMae], [sis_HostName], [sis_SistemaOperacional], [sis_Data_Instalacao], [sis_Dominio], [sis_Arquitetura], [sis_Usuario_Atual], [rede_IPV4], [rede_MacAddress], [rede_DNS], [hw_Memoria_RAM], [hw_HD], [nome_tecnico], [local_mapeamento]
  
     try:
         conn = connect()
  
         cursor = conn.cursor()
-        cursor.execute(query_local, args_local)
-##        conn.commit()
-        
-        print(cursor.lastrowid)
-
-        fk = cursor.lastrowid
-        print('fk: ', fk)
-        query_sis = """INSERT INTO sistema_info(mapeamento_id_mapeamento, sis_data_instalacao)VALUES(%s, %s)"""
-        args_sis = [fk], [sis_Data_Instalacao]
-        cursor.execute(query_sis, args_sis)
-        
+        cursor.execute(query, args)
+ 
         if cursor.lastrowid:
             print('Successo ao inserir maquina atual')
         else:
             print('erro ao ins nome')
  
-##        conn.commit()
-    except Error as erro:
+        conn.commit()
+    except Error as error:
 
         print(erro)
  
@@ -220,4 +208,4 @@ def insert_nome(hw_Processador, sis_HostName, hw_Modelo_PlacaMae, hw_Fabricante_
         cursor.close()
         conn.close()
 
-insert_nome(hw_Processador, sis_HostName(), hw_Modelo_PlacaMae, hw_Fabricante_PlacaMae, hw_NS_PlacaMae, sis_SistemaOperacional, sis_Data_Instalacao(), rede_IPV4, rede_MacAddress, rede_DNS, hw_Memoria_RAM, hw_HD, sis_Dominio, sis_Arquitetura, sis_Usuario_Atual, nome_tecnico, map_local)
+insert_nome(hw_Processador(), hw_Fabricante_PlacaMae(), hw_Modelo_PlacaMae(), hw_NS_PlacaMae(), sis_HostName(), sis_SistemaOperacional(), sis_Data_Instalacao(), sis_Dominio(), sis_Arquitetura(), sis_Usuario_Atual(), rede_IPV4(), rede_MacAddress(), rede_DNS(), hw_Memoria_RAM(), hw_HD(), nome_tecnico, local_mapeamento)
